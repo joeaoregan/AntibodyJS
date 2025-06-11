890001fb8044fa3b0bed9ab31eaf83abb02be9f2
joeaoregan <joe.oregan@gmail.com>
2025-06-11 11:41:10 +0100

    update dependencies

--- server.js
+++ server.js
@@ -11,13 +11,15 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = require('http').createServer(app);
 const os = require('os');
-const dns = require('dns');
+// const dns = require('dns');
 
 console.log("Antibody JS by Joe O'Regan");
 
-let addr=dns.lookup(os.hostname(), function(err,add,fam){
-	console.log("Server running on " +add+":"+ port);
-});
+// let addr=dns.lookup(os.hostname(), function(err,add,fam){
+// 	console.log("Server running on " +add+":"+ port);
+// });
+
+// console.log("Server runnning on http://localhost:3000");
 
 
 console.log("Platform: " + os.platform() + " Architecture: " + os.arch() + " Hostname: " + os.hostname());
@@ -43,4 +45,6 @@ console.log("Platform: " + os.platform() + " Architecture: " + os.arch() + " Hos
 
 app.use(express.static('static'));
 
-server.listen(port);
+server.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
