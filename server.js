cfe75f25cf59163b6d3ce9700a3cdca552d7058c
joeaoregan <joe.oregan@gmail.com>
2026-03-24 14:22:17 +0000

    refactor: clean up server.js and update static file serving to use 'docs'

--- server.js
+++ server.js
@@ -5,45 +5,17 @@
 	Antibody - Server
 */
 const express = require('express');
-// socketio = require('socket.io');
+const os = require('os');
 
 const port = process.env.PORT || 3000;
 const app = express();
-const server = require('http').createServer(app);
-const os = require('os');
-// const dns = require('dns');
 
 console.log("Antibody JS by Joe O'Regan");
+console.log(`Platform: ${os.platform()} | Architecture: ${os.arch()} | Hostname: ${os.hostname()}`);
 
-// let addr=dns.lookup(os.hostname(), function(err,add,fam){
-// 	console.log("Server running on " +add+":"+ port);
-// });
+app.use(express.static('docs'));
 
-// console.log("Server runnning on http://localhost:3000");
-
-
-console.log("Platform: " + os.platform() + " Architecture: " + os.arch() + " Hostname: " + os.hostname());
-
-
-// scores json
-// sychronous
-// const fs = require('fs');
-// let rawdata = fs.readFileSync('static/scores.json');
-// let scores = JSON.parse(rawdata);
-// console.log(scores);
-
-// promise-based / asynchronous
-// const fs = require('fs/promises');
-// fs.readFile('static/scores.json')
-// .then((data) => {
-// 	console.log(JSON.parse(data));
-// })
-// .catch((error) => {
-// 	console.log(error);
-// });
-
-
-app.use(express.static('static'));
+const server = require('http').createServer(app);
 
 server.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
